ca1e4186a6c7896f3041e383d8e50182b39d5cec
Arvind Thakur <arvindnaththakur@gmail.com>
2020-09-23 19:04:10 +0100

    Added theme support using Prop.Render

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,31 +1,39 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { ThemeConsumer } from "../contexts/theme";
 
 export default function Nav() {
   return (
-    <nav className="row space-between">
-      <ul className="row nav">
-        <li>
-          <NavLink
-            exact
-            activeClassName="selected"
-            to="/"
-            className="nav-link"
-          >
-            Top
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            exact
-            activeClassName="selected"
-            to="/new"
-            className="nav-link"
-          >
-            New
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <ThemeConsumer>
+      {({ theme, toggleTheme }) => (
+        <nav className="row space-between">
+          <ul className="row nav">
+            <li>
+              <NavLink
+                exact
+                activeClassName="selected"
+                to="/"
+                className="nav-link"
+              >
+                Top
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                exact
+                activeClassName="selected"
+                to="/new"
+                className="nav-link"
+              >
+                New
+              </NavLink>
+            </li>
+          </ul>
+          <button className="btn-clear btn-nav" onClick={toggleTheme}>
+            {theme === "light" ? "🔦" : "💡"}
+          </button>
+        </nav>
+      )}
+    </ThemeConsumer>
   );
 }
--- app/index.js
+++ app/index.js
@@ -7,21 +7,34 @@ import {
   Switch,
 } from "react-router-dom";
 import Nav from "./components/Nav";
+import { ThemeProvider } from "./contexts/theme";
 import "./index.css";
 
 class App extends React.Component {
+  state = {
+    theme: "light",
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === "light" ? "dark" : "light"
+      }));
+    },
+  };
   render() {
     return (
       <Router>
-        <div className="container">
-          <Nav />
-          <Switch>
-            <Route exact path="/" render={() => <h1>Top</h1>} />
-            <Redirect from="/top" to="/" />
-            <Route path="/new" render={() => <h1>New</h1>} />
-            <Route render={() => <h1>404</h1>} />
-          </Switch>
-        </div>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+              <Switch>
+                <Route exact path="/" render={() => <h1>Top</h1>} />
+                <Redirect from="/top" to="/" />
+                <Route path="/new" render={() => <h1>New</h1>} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </div>
+          </div>
+        </ThemeProvider>
       </Router>
     );
   }
